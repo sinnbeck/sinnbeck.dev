2336a4701adf3b383466dfa57acb35194f36d83f
René Sinnbeck <rs@aalund.com>
2021-12-17 12:06:40 +0100

    add summary

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,19 @@
 
 @section('content')
     @foreach ($posts as $post)
-        <article>
+        <article class="mb-4">
             <h1 class="text-2xl">
                 <a href="/posts/{{$post->slug}}" class="hover:underline hover:text-indigo-700">
                     {{$post->title}}
                 </a>
             </h1>
             <em class="italic text-gray-900 text-sm">Published at: {{$post->published_at->toDateString()}}</em>
-            <p class="py-2 text-gray-100">
+            <p class="py-2 text-gray-800">
                 {{$post->summary}}
             </p>
         </article>
+        @if(!$loop->last)
+            <hr class="mb-4"/>
+        @endif
     @endforeach
 @endsection
