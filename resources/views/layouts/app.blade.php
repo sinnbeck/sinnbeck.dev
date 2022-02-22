d5fa30b0245e0da5ef51d49de4b79d13c7fb8f53
René Sinnbeck <rene.sinnbeck@gmail.com>
2022-02-22 20:36:17 +0100

    add copy to clipboard button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <link href="/css/app.css" rel="stylesheet"/>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg">
     <script defer data-domain-id="{{config('app.fairanalytics')}}" src="https://fairdatacenter.de/cdn/fair.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/clipboard@2.0.10/dist/clipboard.min.js" defer></script>
+
 </head>
 <body class="bg-slate-100">
 <div class="bg-slate-900 hidden border-b border-amber-500 mb-8 ">
@@ -18,7 +20,6 @@
         <div class="flex flex-col lg:flex-row justify-between lg: items-center py-6 md:justify-start md:space-x-10">
             <div class="flex  justify-start lg:w-0 lg:flex-1">
                 <a href="/" class="flex items-center gap-3 text-indigo-600">
-                    <img class="h-8 w-auto sm:h-10 hidden" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path d="M12 14l9-5-9-5-9 5 9 5z" />
                         <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
@@ -49,5 +50,58 @@
 <div class="container max-w-4xl mx-auto px-4">
     @yield('content')
 </div>
+<script>
+
+const copyButtonLabel = "Copy Code";
+const copyButtonLabel2 = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+</svg>
+`
+
+let blocks = document.querySelectorAll("pre");
+
+blocks.forEach((block) => {
+  // only add button if browser supports Clipboard API
+  if (navigator.clipboard) {
+    let button = document.createElement('button');
+    button.innerHTML = copyButtonLabel;
+    button.addEventListener('click', copyCode);
+    block.appendChild(button);
+  }
+});
+
+async function copyCode(event) {
+  const button = event.srcElement;
+  const pre = button.parentElement;
+  let newPre = pre.cloneNode(true);  
+
+  let code = pre.querySelector('code');
+  let lines = [].slice.call(code.children);
+  let texts = []
+  lines.forEach(function(item) {
+      let parts = [].slice.call(item.children);
+      let temp = ''
+      parts.forEach(function(part) {
+        if (!part.classList.contains('line-number')) {
+            temp += part.innerText
+        }
+        
+      })
+      texts.push(temp)
+    
+      
+  })
+
+  const final = texts.join("\n")
+  let text = code.innerText;
+  await navigator.clipboard.writeText(final);
+  
+  button.innerText = "Code Copied";
+  
+  setTimeout(()=> {
+    button.innerText = copyButtonLabel;
+  },1000)
+}
+</script>
 </body>
 </html>
